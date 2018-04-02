5ed9409e3f8a598a7908001cc5727fcfc9b83ddf
daheng <adamwang0705@gmail.com>
2018-04-01 23:36:23 -0400

    first component

--- learn_suc/predict/urls.py
+++ learn_suc/predict/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'predict'
 urlpatterns = [
     path('helloworld/', views.helloworld, name='helloworld'),
-    path('', views.type_list, name='default'),
-    path('index/', views.type_list, name='index'),
+    path('', views.index, name='default'),
+    path('index/', views.index, name='index'),
     path('type/summary/', views.type_summary, name='type_summary'),
     path('type/<int:type_id>/', views.type_detail, name='type_detail'),
     path('item/summary/', views.item_summary, name='item_summary'),
--- learn_suc/predict/views.py
+++ learn_suc/predict/views.py
@@ -5,17 +5,9 @@ from .models import Type, Item, Embedding
 
 # Create your views here.
 
-"""
-For debug
-"""
+"""For debug"""
 def helloworld(request):
     return HttpResponse('Hello World. LearnSUC online powered by Django.')
-
-
-def type_list(request):
-    types = Type.objects.all()
-    context = {'types': types}
-    return render(request, 'predict/types.html', context)
 
 
 def type_summary(request):
@@ -52,9 +44,29 @@ def embedding_detail(request, item_id):
     embedding = Embedding.objects.get(pk=item_id)
     return HttpResponse('Item {}: -embedding: {};'.format(item_id, embedding.embedding))
 
-"""
-Public interface
-"""
-# def index(request):
-#     authors = Item.objects.filter(type__name='author')[:500]
 
+"""Public interface"""
+# LINE: Large-scale Information Network Embedding
+# Behavior id: 347613
+default_behavior = [2767459, 2906020, 2972072, 3078788, 3095165, 3169331, 3249647, 3255758, 3254437, 3263395, 1382595,
+                    1492387, 1980713, 267563, 569799, 888462, 2127379, 882200, 1391569, 1605587, 178992, 641289,
+                    1779357, 47992, 1986676, 1811546, 1353460]
+
+default_rate = 90.0
+
+def index(request):
+    # Reset to default behavior
+    request.session.setdefault('behavior', default_behavior)
+    request.session.setdefault('rate', default_rate)
+
+    behavior_info = []
+    for item_id in request.session.get('behavior'):
+        item = Item.objects.get(pk=item_id)
+        behavior_info.append({'name': item.name, 'type': item.type.name})
+
+    authors = Item.objects.filter(type__name='author').order_by('name')[:20]
+    context = {'displaying_type': 'author',
+               'displaying_items': authors,
+               'behavior_info': behavior_info,
+               'rate': request.session.get('rate')}
+    return render(request, 'predict/compose_behavior.html', context)
